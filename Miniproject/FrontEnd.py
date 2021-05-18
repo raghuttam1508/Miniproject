27126a9e11ad748919feb957317d269aad14aa43
Raghuttam Parvatikar <68182676+raghuttam1508@users.noreply.github.com>
2021-05-18 11:15:09 +0530

    Updatation to the previous code

--- Miniproject/FrontEnd.py
+++ Miniproject/FrontEnd.py
@@ -5,37 +5,29 @@ from pandas.core.frame import DataFrame
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from plotly import graph_objs as go
 import altair as alt
 import os
 import string
 
 from streamlit.uploaded_file_manager import UploadedFile
-
-#def xl_key(kee, database):
-    #if database == 'Product Sets':
-    #    try :
-    #        product_values = set.loc[kee]
-    #    except KeyError:
-    #        return None,None
-
-    #    nm = product_values['Name']
-    #    p_id = product_values['ID']
-    #    return nm, p_id
-
+  
+sets = pd.read_excel("adharr.xlsx")
 
 # Add a selectbox to the sidebar:
-st.sidebar.header('Your place or mine?')
-rad = st.sidebar.radio('Select Your Domain: ',['Home', 'Customer Desk','Client Desk','New Customer?','About us'])
+st.sidebar.header('Navigation')
+rad = st.sidebar.radio('Select Your Domain: ',['Home', 'Customer Desk','Client Desk','About us'])
 
 if rad == 'Home':
     st.title("WELCOME TO THE RESUME OF YOUR PRODUCT")
-    st.header("You are at home!")
-    st.write(""" 
+    st.markdown(""" 
     # Our Product: 
-    Yap yap lines
-    """)
-    st.image("MacW.jpg",caption='The future is Here',use_column_width=True)
+    ### **Welcome to the first and the only quality asuarance website, where we provide you with the CV of the prodduct you bought. So hop on to the customer panel in the sidebar now!** <br> 
+    ### **As we are still in ecperimental stage, we only have a few items in our database. So, be patient, we'll pick each and everyone UP!**
+    ### **We intend to develop a prototype for a system where a consumer can ensure the product they are buying are Original to the manufacturer, matches the same level of claimed purity and also tracking the adulteration in the constituents of the product.**
+    """,True)
+    st.image("rebrand.jpg",caption='The future is Here',use_column_width=True)
 
 if rad == 'Customer Desk':
     st.title("CUSTOMER DESK")   
@@ -43,62 +35,51 @@ if rad == 'Customer Desk':
     ### Welcome to our Lobby! <br>
 
     """,True)
-    user_c = st.text_input('USERNAME or CUSTOMER ID')
-    user_p = st.text_input('PASSWORD',type = 'password')
-    sub = st.button('Login')
-    st.markdown("""
-    ### **New to our Page?, no worries Register by clicking New User!** <br> <br>    
-    """,True)    
-
-    if sub:
-        if user_c == '123'and user_p == '123':
-            st.write("""
+    st.write("""
             ## **ENTER YOUR PRODUCT ID/BARCODE NUMBER**
             # """)
-            enter_id,submit = st.beta_columns([3,1])
-            enterID = enter_id.text_input('Input')
-            u_submit = submit.button("Submit")
-            #add code to check from database and printing the row
-            
-        else:
-            st.error('Wrong Credentials!!')  
-
-if rad == 'New Customer?':
-        st.title('New User')
-        first,last = st.beta_columns(2)
-        Fname = first.text_input('FIRST NAME')
-        Lname = last.text_input('LAST NAME')
-        st.write("""
-        ### AGE:
-        """)
-        age = st.slider('Age', min_value= 14)
-        address = st.text_area('ADDRESS:', height=14, max_chars= 50)
-        id = st.text_input('Valid ID number:')
-        first_1,last_1 = st.beta_columns([2.5,1])
-        email = first_1.text_input('Valid Email ID')
-        nu = last_1.text_input('Phone Number')
-        new_user_sub_button = st.button('Submit')
-        # Add code linking to database, adding users
+    enterID = st.text_input('Input')
+    u_submit = st.button("Submit")
+    #add code to check from database and printing the row.
         
 if rad == 'Client Desk':
     st.title("CLIENT DESK") 
     st.markdown(""" 
     ### Welcome to our Lobby! <br>
-
     """,True)
     client_c = st.text_input('USERNAME OR CLIENT ID')
-    client_p = st.text_input('PASSWORD',type='password')
+    client_p = st.text_input('PASSWORD',type='password',)
     client_sub = st.button('Login')
 
     if client_sub:
         if client_c == '123' and client_p == '123':
-            sets = pd.read_excel("adharr.xlsx", index_col="UID")
-            a1,a2 = st.beta_columns(2)
-            a1.subheader("Data")
-            a1.write(sets)
+            #Add code for connecting DB for LOGIN window
+            st.header('**Our Data Sets**')
+            st.table(sets) 
+
+            graphs = st.sidebar.selectbox("What kind of visualisations?",['Future Quaotations','Market Demand VS Supply'], index=0) 
+            
+            if graphs == 'Future Quaotations':
+                layout = go.Layout(
+                    xaxis = dict(range=[0,16.000]),
+                    yaxis = dict(range=[0,2100.000])
+                )
+                fig = go.Figure(sets=go.Scatter(x = sets['UID'], y = sets['NAME'],mode='markers' ),layout = layout)
+                st.plotly_chart(fig)
+            
+            if graphs == 'Market Demand VS Supply':
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                plt.figure(figsize = (10,5))
+                plt.scatter(sets['UID'],sets['NAME'])
+                plt.ylim(0)
+                plt.ylabel('NAME')
+                plt.xlabel('UID')
+                plt.tight_layout()
+                st.pyplot()
+            
+
         else:
             st.error('Wrong Credentials!')
-
 
 if rad == 'About us':   
     st.title('TEAM MEMBERS')
@@ -111,17 +92,7 @@ if rad == 'About us':
     ## **Project Objectives**
     ''')
 
-
-
-
-
 #st.file_uploader('Dataset')
-
-
-
-
-
-
 
 #st.line_chart(data)
 #st.area_chart(data)
